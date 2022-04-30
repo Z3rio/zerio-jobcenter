ebff4d5bd7551e614fa0b40498744c90d47d315f
Zerio <54480523+Z3rio@users.noreply.github.com>
2022-04-30 12:52:28 +0200

    Add files via upload

---
+++ ESX/zerio-jobcenter/config.lua
@@ -0,0 +1,57 @@
+Config = {}
+
+Config.Locations = {
+    {
+        position = vector3(-265.3164, -963.5999, 31.2159),
+        jobs = {
+            {
+                name = "taxi",
+                label = "Taxi",
+                description = "Drive NPC's or real players around and make money from it",
+                icon = "mdi-taxi"
+            },
+            {
+                name = "garbage",
+                label = "Garbage Collector",
+                description = "Collect garbage around the city and make money from it",
+                icon = "mdi-delete"
+            },
+            {
+                name = "taxi",
+                label = "Taxi",
+                description = "Drive NPC's or real players around and make money from it",
+                icon = "mdi-taxi"
+            },
+            {
+                name = "garbage",
+                label = "Garbage Collector",
+                description = "Collect garbage around the city and make money from it",
+                icon = "mdi-delete"
+            },
+            {
+                name = "taxi",
+                label = "Taxi",
+                description = "Drive NPC's or real players around and make money from it",
+                icon = "mdi-taxi"
+            },
+            {
+                name = "garbage",
+                label = "Garbage Collector",
+                description = "Collect garbage around the city and make money from it",
+                icon = "mdi-delete"
+            },
+            {
+                name = "taxi",
+                label = "Taxi",
+                description = "Drive NPC's or real players around and make money from it",
+                icon = "mdi-taxi"
+            },
+            {
+                name = "garbage",
+                label = "Garbage Collector",
+                description = "Collect garbage around the city and make money from it",
+                icon = "mdi-delete"
+            },
+        }
+    }
+}

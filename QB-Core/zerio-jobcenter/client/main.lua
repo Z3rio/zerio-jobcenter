b476c561e48a5f1aa0b526c41e7102cce3ef7de8
Chas Rivers <chas.rivers1001@gmail.com>
2023-02-09 20:54:22 -0500

    added prices, grades, default qb jobs

--- QB-Core/zerio-jobcenter/client/main.lua
+++ QB-Core/zerio-jobcenter/client/main.lua
@@ -8,9 +8,9 @@ CreateThread(function()
         while true do
             local playerpos = GetEntityCoords(PlayerPedId())
 
-            local closestpos2, closestidx2, closestdist2 = nil,nil,math.huge
+            local closestpos2, closestidx2, closestdist2 = nil, nil, math.huge
 
-            for i,v in pairs(Config.Locations) do
+            for i, v in pairs(Config.Locations) do
                 local dist = #(playerpos - v.position)
                 if dist < closestdist2 then
                     closestdist2 = dist
@@ -27,19 +27,19 @@ CreateThread(function()
         end
     end)
 
-    RegisterNUICallback("close", function(data,cb)
+    RegisterNUICallback("close", function(data, cb)
         SetNuiFocus(false, false)
         isopen = false
         TriggerScreenblurFadeOut(250)
         cb("ok")
     end)
 
-    RegisterNUICallback("getjob", function(data,cb)
+    RegisterNUICallback("getjob", function(data, cb)
         TriggerServerEvent("zerio-jobcenter:server:setjob", data.jobindex, closestidx)
         cb("ok")
     end)
 
-    for i,v in pairs(Config.Locations) do
+    for i, v in pairs(Config.Locations) do
         local blip = AddBlipForCoord(v.position.x, v.position.y, v.position.z)
         SetBlipSprite(blip, 351)
         SetBlipAsShortRange(blip, true)
@@ -51,27 +51,28 @@ CreateThread(function()
     end
 
     while true do
-        if closestpos ~= nil and closestdist < 2.0 and isopen == false then
-            AddTextEntry('jobcenterhelp', "~INPUT_CONTEXT~ - Open Jobcenter") 
-            SetFloatingHelpTextWorldPosition(1, closestpos) 
-            SetFloatingHelpTextStyle(1, 1, 2, -1, 3, 0) 
-            BeginTextCommandDisplayHelp('jobcenterhelp') 
-            EndTextCommandDisplayHelp(2, false, false, -1) 
+        if closestpos ~= nil and closestdist < 10.0 and isopen == false then
+            AddTextEntry('jobcenterhelp', "~INPUT_CONTEXT~ - Open Jobcenter")
+            SetFloatingHelpTextWorldPosition(1, closestpos)
+            SetFloatingHelpTextStyle(1, 1, 2, -1, 3, 0)
+            DrawMarker(23, closestpos.x, closestpos.y, closestpos.z-1, 0.0, 0.0, 0.0, 0.0, 180.0, 0.0, 2.0, 2.0, 2.0, 0, 100, 155, 85, false, true, 2, nil, nil, false)
+            BeginTextCommandDisplayHelp('jobcenterhelp')
+            EndTextCommandDisplayHelp(2, false, false, -1)
 
-            if closestdist < 1.0 and IsControlJustReleased(0,38) then
+            if closestdist < 1.0 and IsControlJustReleased(0, 38) then
                 SendNUIMessage({
                     action = "open",
                     jobs = Config.Locations[closestidx].jobs,
                     job = QBCore.Functions.GetPlayerData().job.name
                 })
-                SetNuiFocus(true,true)
+                SetNuiFocus(true, true)
                 isopen = true
                 TriggerScreenblurFadeIn(500)
             end
 
             Citizen.Wait(0)
         else
-            Citizen.Wait(1000)
+            Citizen.Wait(100)
         end
     end
 end)
--- QB-Core/zerio-jobcenter/server/main.lua
+++ QB-Core/zerio-jobcenter/server/main.lua
@@ -5,7 +5,9 @@ AddEventHandler("zerio-jobcenter:server:setjob", function(jobindex, jobcenteridx
     local Player = QBCore.Functions.GetPlayer(source)
 
     if Config.Locations[jobcenteridx] and Config.Locations[jobcenteridx].jobs[jobindex+1] then
-        Player.Functions.SetJob(Config.Locations[jobcenteridx].jobs[jobindex+1].name, 0)
+        -- Player.Functions.SetJob(Config.Locations[jobcenteridx].jobs[jobindex+1].name, 0)
+        Player.Functions.SetJob(Config.Locations[jobcenteridx].jobs[jobindex+1].name, Config.Locations[jobcenteridx].jobs[jobindex+1].grade)
+        Player.Functions.RemoveMoney("bank", Config.Locations[jobcenteridx].jobs[jobindex+1].price, "jobcenter-new-job " .. Config.Locations[jobcenteridx].jobs[jobindex+1].name .. "Grade: " .. Config.Locations[jobcenteridx].jobs[jobindex+1].grade)
         TriggerClientEvent("QBCore:Notify", Player.PlayerData.source, "You changed your job to " .. Config.Locations[jobcenteridx].jobs[jobindex+1].label)
     end
 end)
